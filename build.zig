a36031091249d39d05caaa49cfe9992e465baa24
Samuel Mott <samuel.e.mott@gmail.com>
2024-10-27 21:29:18 +0000

    build!: build examples separately

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -10,7 +10,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    // payload.linkLibC();
     payload.linkSystemLibrary("luajit-5.1");
     b.installArtifact(payload);
 
@@ -35,6 +34,7 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    // TODO: Refactor the test builds
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -48,41 +48,41 @@ pub fn build(b: *std.Build) void {
     const unit_test_step = b.step("test-unit", "Run unit tests");
     unit_test_step.dependOn(&run_exe_unit_tests.step);
 
-    const run_exe_integration_tests = b.addRunArtifact(exe_unit_tests);
+    var examples_step = b.step("examples", "Build examples exes and libs to try out");
 
-    const c_exe_tests = [_][]const u8{
-        "basic-print-loop",
-    };
-
-    inline for (c_exe_tests) |test_name| {
-        const c_test = b.addExecutable(.{
-            .name = test_name,
+    var exe_dir = try std.fs.cwd().openDir("examples/exe/", .{ .iterate = true });
+    defer exe_dir.close();
+    var iter = exe_dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind != .file) {
+            continue;
+        }
+        const c_exe = b.addExecutable(.{
+            .name = entry.name,
             .target = target,
             .optimize = optimize,
         });
-        const file = std.fmt.comptimePrint("tests/{s}.c", .{test_name});
-        c_test.addCSourceFile(.{ .file = .{ .cwd_relative = file }, .flags = &[_][]const u8{"-std=c99"} });
-        c_test.linkLibC();
-        run_exe_integration_tests.step.dependOn(&b.addInstallArtifact(c_test, .{}).step);
+        const file = try std.fmt.allocPrint(b.allocator, "examples/exe/{s}", .{entry.name});
+        c_exe.addCSourceFile(.{ .file = .{ .cwd_relative = file }, .flags = &[_][]const u8{"-std=c99"} });
+        c_exe.linkLibC();
+        examples_step.dependOn(&b.addInstallArtifact(c_exe, .{}).step);
     }
 
-    const c_lib_tests = [_][]const u8{
-        "pic-hello",
-    };
-
-    inline for (c_lib_tests) |test_name| {
-        const c_test = b.addSharedLibrary(.{
-            .name = test_name,
+    var lib_dir = try std.fs.cwd().openDir("examples/lib/", .{ .iterate = true });
+    defer lib_dir.close();
+    iter = lib_dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind != .file) {
+            continue;
+        }
+        const c_lib = b.addSharedLibrary(.{
+            .name = entry.name,
             .target = target,
             .optimize = optimize,
         });
-        const file = std.fmt.comptimePrint("tests/{s}.c", .{test_name});
-        c_test.addCSourceFile(.{ .file = .{ .cwd_relative = file }, .flags = &[_][]const u8{ "-std=c99", "-fPIC" } });
-        c_test.linkLibC();
-        run_exe_integration_tests.step.dependOn(&b.addInstallArtifact(c_test, .{}).step);
+        const file = try std.fmt.allocPrint(b.allocator, "examples/lib/{s}", .{entry.name});
+        c_lib.addCSourceFile(.{ .file = .{ .cwd_relative = file }, .flags = &[_][]const u8{ "-std=c99", "-fPIC" } });
+        c_lib.linkLibC();
+        examples_step.dependOn(&b.addInstallArtifact(c_lib, .{}).step);
     }
-
-    run_exe_integration_tests.has_side_effects = true;
-    const integration_test_step = b.step("test-integration", "Run integration tests");
-    integration_test_step.dependOn(&run_exe_integration_tests.step);
 }
