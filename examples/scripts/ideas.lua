cfc83911903bedebdbce213f4aa6e6050ee08286
Samuel Mott <samuel.e.mott@gmail.com>
2024-11-03 15:09:24 +0000

    docs: more ideas around handling entry/exit hooks in Lua
    Lua metaprogramming can be used to make things convenient for the end
    user. Only concern is introducing too much "magic", and therefore
    complexity. Another option is having a clean seperation between data and
    functions.

--- examples/scripts/ideas.lua
+++ examples/scripts/ideas.lua
@@ -1,5 +1,8 @@
 -- This file is for me to explore what I'd like the scripting API to be like
 
+-- Setting entry/exit overrides trigger this, so ignoring it
+---@diagnostic disable: duplicate-set-field
+
 -- How can I take advantage of FFI for a lot of this?
 -- ffi.cdef() won't get hook_me() from basic-print-loop.c, and it can't search for functions
 -- ffi.load() takes care of loading new modules
@@ -11,22 +14,69 @@
 --   I can offload that work to ffi, e.g., for declaring function pointers with intuitive types
 local ffi = require("ffi")
 
+-- Generate a function signature based on the given C argument and return types.
+-- This can be passed to `ffi.cast()` to create direct refernces to native functions.
+-- Example:
+--   `Signature({"int", "char*"}, "void*")` => `"void*(*)(int, char*)"`
+--
+---@param argument_types string[] list of argument types
+---@param return_type string return type
+function Signature(argument_types, return_type)
+	local args = "void"
+	if argument_types then
+		args = string.format(string.rep("%s,", #argument_types):sub(1, -2), table.unpack(argument_types))
+	end
+	return string.format("%s(*)(%s)", return_type or "void", args)
+end
+
 ---@class Module
 ---@field base integer starting address of the module
 ---@field size integer total size in bytes of the module
 ---@field path string? path to the module on the filesystem
+Module = {}
+Module.__index = Module
 
 ---@class Function
 ---@field addr integer absolute address of the function
 ---@field argument_types string[] array of the function's argument types in order
 ---@field return_type string return type of the function
+---@field entry function? entry hook function
+---@field exit function? exit hook function
+---@field private _data table
 Function = {
-	_func = nil,
 	__call = function(self, ...)
-		return self._func(...)
+		return self._data._func(...)
+	end,
+	original = function(self, ...)
+		-- TODO: Call original, unhooked version
+		return self.__func(...)
 	end,
 }
 Function.__index = Function
+
+function Function:__newindex(key, value)
+	local update_func = false
+	if key == "entry" or key == "exit" then
+		if value == nil or type(value) == "function" then
+			-- TODO: Communicate hooking to server
+		else
+			error("Attempt to assign non-function value to " .. key)
+		end
+	elseif key == "addr" or key == "argument_types" or key == "return_type" then
+		update_func = true
+	end
+	rawset(self._data, key, value)
+	if update_func and self.addr then
+		-- TODO: Communicate hook update to server
+		local signature = Signature(self.argument_types, self.return_type)
+		---@diagnostic disable-next-line: param-type-mismatch
+		self._data["_func"] = ffi.cast(signature, self.addr)
+	end
+end
+
+function Function:__index(key)
+	return self._data[key]
+end
 
 -- Create a new Function object to call native functions.
 -- `argument_types` and `return_type` use standard C types, e.g., `int`, `uint64_t`, `char*`, etc
@@ -34,52 +84,48 @@ Function.__index = Function
 ---@param addr integer absolute address of the function
 ---@param argument_types string[] array of the function's argument types in order
 ---@param return_type string return type of the function
+---@param entry function? entry hook function
+---@param exit function? exit hook function
 ---
 ---@return Function
-function Function.new(addr, argument_types, return_type)
-	local self = setmetatable({}, Function)
-	self.addr = addr
+function Function.new(addr, argument_types, return_type, entry, exit)
+	local self = setmetatable({
+		_data = {
+			_func = nil,
+		},
+	}, Function)
 	self.argument_types = argument_types
 	self.return_type = return_type
-
-	local args = ""
-	if self.argument_types then
-		args = string.format(string.rep("%s,", #self.argument_types):sub(1, -2), table.unpack(self.argument_types))
-	end
-	local signature = string.format("%s(*)(%s)", self.return_type, args)
-
-	---@diagnostic disable-next-line: param-type-mismatch
-	self._func = ffi.cast(signature, self.addr)
+	self.addr = addr
+	self.entry = entry
+	self.exit = exit
 
 	return self
 end
 
--- Return a the module with the matching `name`.
--- If `name` is `nil`, return the list of all loaded modules.
+-- Return the module whose name matches `name`.
+-- If `name` is `nil`, return a list of all loaded modules.
 --
 ---@param name string?
 ---
 ---@return Module[] | Module
-function GetModules(name)
-	return {}
+function Module.get(name)
+	-- Call out to payload server?
+	return setmetatable({}, Module)
 end
 
 -- Gets the address of the export `name` from the chosen module.
--- If `module` is `nil`, it defaults to searching all loaded modules.
 -- If `name` is `nil`, it returns all exports.
 --
----@param module string?
 ---@param name string?
 ---
 ---@return integer[] | integer
-function GetExports(module, name)
+function Module:exports(self, name)
+	-- Call out to payload server?
 	return 0
 end
 
 -- Provide ways to:
--- TODO: Define entry hooks
--- TODO: Define exit hooks
--- TODO: Remove hooks
 -- TODO: Read/write registers in hooks
 -- TODO: Override return value
 -- TODO: Get memory ranges
@@ -100,6 +146,47 @@ end
 -- TODO: Interact with SQLite DBs
 -- TODO: Interact with other environments, e.g., Java, ObjC, Python, Ruby, Dotnet, etc
 
+-- Getting a specific module
+local base_module = Module.get("basic-print-loop")
+
+-- Getting a specific function from a module
+local hook_me_addr = base_module:exports("hook_me")
+hook_me_addr = 0x1001610
+
 -- Defining and calling a function
-local hook_me = Function.new(0x1001610, { "int" }, "int")
+local hook_me = Function.new(hook_me_addr, { "int" }, "int")
 print(string.format("hook_me(123) -> %d", hook_me(123)))
+
+-- TODO: Define entry hooks
+-- TODO: Define exit hooks
+-- Hooking the entry point of a function
+-- This involves some meta magic, not sure I'm a fan of the added internal complexity
+hook_me.entry = function(i)
+	print("Overriding argument in hook_me()...")
+	-- Best way to handle overriding arguments?
+	-- I can probably get the state of the arguments in Zig after the callback has executed.
+	print(i)
+	i = 123
+end
+hook_me.exit = function(ret)
+	print(ret)
+	return 123
+end
+-- TODO: Remove hooks
+hook_me.entry = nil
+hook_me.exit = nil
+
+-- TODO: Replace functions
+hook_me.entry = function(i)
+	print("Replacing hook_me()...")
+	-- call original version?
+	print(hook_me:original(123))
+	-- Adding a return prevents calling the original function after, effectively replacing it?
+	return 42
+	-- No return (or return nil) would indicate to execute the function as normal after
+	-- What about native functions that return void (AKA nil)?
+end
+-- Alternatively, be explicit?
+hook_me.replace(function(i)
+	return 42
+end)
